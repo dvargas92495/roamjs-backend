f7a4763ff7190ddfd28e4f9dce2ed4e6fd9845a4
David Vargas <dvargas92495@gmail.com>
2023-03-06 15:26:52 -0500

    fix role name ref

--- main.tf
+++ main.tf
@@ -406,7 +406,7 @@ resource "aws_iam_role" "roamjs_lambda_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "attach" {
-  role       = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.roamjs_lambda_role.name
   policy_arn = aws_iam_policy.lambda_execution_policy.arn
 }
 
